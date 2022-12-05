4d016c63f08519c8fabc0f51fe1b936f4f376617
JYH11 <noinneha@naver.com>
2022-12-05 19:01:20 +0900

    221205

---
+++ src/basic/2420.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long a, b;
+    cin >> a >> b;
+    long long ans = a-b;
+    if (ans<0)
+        cout << -ans;
+    else
+        cout << ans;
+}
